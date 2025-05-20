196eb40b839f53c0d765d61d6cdc800f4a7c9bc1
sadah00 <kimberlysada30@gmail.com>
2025-05-20 17:09:14 +0300

    integers_floats.py

---
+++ integers_floats.py
@@ -0,0 +1,40 @@
+num1=1000
+print(type(num1))
+num2=23.46
+print(type(num2))
+
+x=99
+y=5
+# modulus %
+q=x%y
+print(q)
+
+# floor //
+a=x//y
+print(a)
+
+# ari1=input('enter digit:')
+# ari1=int(ari1)
+# ari2=input('enter digit:')
+# ari2=int(ari2)
+# sum=ari1+ari2
+# print(sum)
+
+
+# Questions
+# Convert a float to an integer with an inbuilt function in Python
+# temp = 56.8926 to 57
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.89 
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.893 
+# Convert the float below to give the results as follows
+# temp=56.8926 to 8.926 
+# NB: Use string  slice & concatenation, but have result as float 
+
+# Attempt questions below. Whether you get the right answer or not, still read the solution explanation.
+# https://realpython.com/quizzes/python-data-types/
+
+
+
+
--- string_methods.py
+++ string_methods.py
@@ -21,5 +21,35 @@ chat="hello"
 chat_re=chat[::-1]
 print(chat_re)
 
+sent="good habits are hard to break!"
+sent1=sent.replace("good","BAD")
+print(sent1)
 
+email="  John.Doe@GMAIL.com "
+str=email.strip()
+rep=str.lower()
+print(rep)
+print(rep[9:18])
 
+first="john"
+first_cap=first.capitalize()
+print(first_cap)
+last="DOE"
+last_cap=last.capitalize()
+print(last_cap)
+hobby=" Reading Books "
+hobby_str=hobby.strip()
+print(hobby_str)
+chat=f"My name is {first_cap} {last_cap} and I love {hobby_str}."
+print(chat)
+
+sente=input('Enter a sentence:')
+sente2=sente.split(' ')
+print(sente2)
+print(len(sente2))
+
+sente_tit=sente.title()
+print(sente_tit)
+
+sente_rev=sente[::-1]
+print(sente_rev)
