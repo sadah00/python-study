544022a9f213807bd1a68cabd28b7363d4142feb
sadah00 <kimberlysada30@gmail.com>
2025-05-23 00:17:28 +0300

    tuples_task.py

---
+++ tuples_task.py
@@ -0,0 +1,37 @@
+# 1. numbers = (10, 20, 30, 40, 50)Add 60 to the end,Replace 30 with 35.
+numbers = (10, 20, 30, 40, 50)
+numbers=list(numbers)
+print(type(numbers))
+numbers.append(60)
+print(numbers)
+numbers[2]=35
+numbers=tuple(numbers)
+print(numbers)
+# 2. values = (15, 5, 30, 25, 10) arrange the elements in ascending order.
+values = (15, 5, 30, 25, 10)
+values=list(values)
+values.sort()
+values=tuple(values)
+print(values)
+# 3. fruits = ("apple", "banana", "cherry", "banana", "mango", "banana")
+# Count occurrences of "banana",Remove all occurrences of "banana".
+fruits = ("apple", "banana", "cherry", "banana", "mango", "banana")
+print(fruits.count("banana"))
+fruits=set(fruits)
+fruits.remove("banana")
+fruits=tuple(fruits)
+print(fruits)
+# 4. names = ("Alice", "Bob", "Charlie", "David") Reverse the order of elements using sort method.
+names = ("Alice", "Bob", "Charlie", "David")
+names=list(names)
+names.sort(reverse=True)
+names=tuple(names)
+print(names)
+# 5. colors = ("red", "blue", "green")add "yellow" at index 1,Extend with ["purple", "orange"]
+colors = ("red", "blue", "green")
+colors=list(colors)
+colors.insert(1,"yellow")
+colors.append("purple")
+colors.append("orange")
+colors=tuple(colors)
+print(colors)
