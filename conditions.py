f30feb135ab8a582e6692d512da704a8b11e1674
sadah00 <kimberlysada30@gmail.com>
2025-05-26 17:03:36 +0300

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,21 @@
+a=20
+b=50
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+# write a program that prints "pass" if marks are 50 or above ,otherwise print "fail"
+marks=40
+if marks>=50:
+    print("pass")
+else:
+    print("fail")
+# write an if-else statement that checks if the entered password is correct.
+# if yes print "access granted",otherwise print "access denied".
+password=input('enter password: ')
+word="camptech"
+if password=="camptech":
+    print("access granted")
+else:
+    print("access denied")
--- mydstask.py
+++ mydstask.py
@@ -10,18 +10,17 @@ print(my_ds[3][1])
 my_ds[3][2]["amount"]=90
 print(my_ds)
 # 5. Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually.
-x=my_ds[4]
-y=str(x)
-print(type(y))
-y=y[::-1]
-print(y)
+my_ds[4]=str(my_ds[4])
+print(type(my_ds[4]))
+my_ds[4]=my_ds[4][::-1]
+my_ds[4]=int(my_ds[4])
+print(my_ds)
 #      Hint: Strings can be reversed using [::]
 # 6. Change the name “John” to “Jane” . 
-z=my_ds[5]
-z=list(z)
-z[1]="Jane"
-z=tuple(z)
-print(z)
+my_ds[5]=list(my_ds[5])
+my_ds[5][1]="Jane"
+my_ds[5]=tuple(my_ds[5])
+print(my_ds)
 # You can research or discuss to find the solutions above.
 #         https://realpython.com/quizzes/pybasics-tuples-lists-dicts/ 
 
